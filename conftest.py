925d42e35e1ea9e6b104bf110549553f4f36dd34
Sefi Teshuva <sefit45@gmail.com>
2026-04-24 01:14:48 +0300

    Restore driver fixture and keep Allure screenshot hook

--- conftest.py
+++ conftest.py
@@ -1,6 +1,35 @@
+import os
 import pytest
 import allure
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from utils.screenshot import take_screenshot
+
+
+@pytest.fixture
+def driver(request):
+    browser = getattr(request, "param", "chrome")
+
+    if browser == "chrome":
+        options = ChromeOptions()
+        options.add_argument("--start-maximized")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--disable-gpu")
+    elif browser == "firefox":
+        options = FirefoxOptions()
+    else:
+        raise Exception(f"Browser {browser} is not supported")
+
+    driver = webdriver.Remote(
+        command_executor="http://localhost:4444/wd/hub",
+        options=options
+    )
+
+    driver.implicitly_wait(5)
+    yield driver
+    driver.quit()
 
 
 @pytest.hookimpl(hookwrapper=True)
@@ -13,12 +42,12 @@ def pytest_runtest_makereport(item, call):
 
         if driver:
             test_name = item.name
-
             screenshot_path = take_screenshot(driver, test_name)
 
-            with open(screenshot_path, "rb") as image_file:
-                allure.attach(
-                    image_file.read(),
-                    name=test_name,
-                    attachment_type=allure.attachment_type.PNG
-                )
+            if os.path.exists(screenshot_path):
+                with open(screenshot_path, "rb") as image_file:
+                    allure.attach(
+                        image_file.read(),
+                        name=test_name,
+                        attachment_type=allure.attachment_type.PNG
+                    )
